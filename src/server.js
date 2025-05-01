a4ef55ea615f4005c6bf5af76e45ef04d837bc03
Netmario <crisdeivi2020@gmail.com>
2025-05-01 14:47:03 -0500

    Agregar configuración de dotenv y cors en server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 // index.js
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
+const cors = require('cors');
 const PORT = 3000;
 
+dotenv.config(); // Cargar las variables de entorno desde el archivo .env
+
+app.use(cors({
+  origin: '*', // Permitir todas las solicitudes de origen cruzado
+  credentials: true, // Permitir el envío de cookies y encabezados de autorización
+}));
+
+const app = express();
 app.get('/', (req, res) => {
   res.send('¡Hola mundo desde Express!');
 });
