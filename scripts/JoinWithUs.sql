33e5b6f08de4c083e953bf5b704438342cd542e1
Valentin <mikeallca27@gmail.com>
2025-07-04 22:17:47 -0500

    fix: se edito el sccript de la bd

--- scripts/JoinWithUs.sql
+++ scripts/JoinWithUs.sql
@@ -103,19 +103,19 @@ CREATE TABLE Notifications (
 -- Insertar registros en la tabla de Usuarios
 INSERT INTO Users (FullName, BirthDate, Phone, DNI, Email, PasswordHash, Role)
 VALUES 
-('Juan Pérez', '1990-05-15', '123456789', '12345678A', 'juan.perez@example.com', 'hashedpassword1', 'user'),
+('Juan Pérez', '1990-05-15', '123456789', '12345678A', 'juan.perez@example.com', 'hashedpassword1', 'admin'),
 ('Ana Martínez', '1985-07-22', '987654321', '98765432B', 'ana.martinez@example.com', 'hashedpassword2', 'organizer'),
 ('Carlos García', '2000-03-30', '555555555', '12365478C', 'carlos.garcia@example.com', 'hashedpassword3', 'user'),
 ('Laura Sánchez', '1995-12-10', '666666666', '65498723D', 'laura.sanchez@example.com', 'hashedpassword4', 'admin'),
-('Pedro Fernández', '1982-11-25', '777777777', '32198765E', 'pedro.fernandez@example.com', 'hashedpassword5', 'user');
+('Pedro Fernández', '1982-11-25', '777777777', '32198765E', 'pedro.fernandez@example.com', 'hashedpassword5', 'helper');
 
 -- Insertar registros en la tabla de Eventos
 INSERT INTO Events (OrganizerId, Title, Description, StartDate, EndDate, Address, Latitude, Longitude, Visibility, Categories, BannerUrl, VideoUrl, Status, Capacity)
 VALUES
 (2, 'Concierto de Rock', 'Un evento de música rock en vivo', '2025-06-10 18:00:00', '2025-06-10 22:00:00', 'Avenida Central 123, Ciudad', 40.7128, -74.0060, 'public', 'Música', 'https://res.cloudinary.com/dlbvvaqsq/image/upload/v1747850787/f2a830ea-d3f6-41dc-9f2a-199add80b380.png', 'https://youtu.be/NQNF2qcbY4k?si=YEwrSsj5zqRZJQmp', 'published', 500),
-(2, 'Conferencia de Tecnología', 'Charlas sobre innovación tecnológica', '2025-07-15 09:00:00', '2025-07-15 17:00:00', 'Calle de la Innovación 456, Ciudad', 40.7128, -74.0060, 'public', 'Tecnología', 'https://res.cloudinary.com/dlbvvaqsq/image/upload/v1747850867/1e6236c8-d88d-43a6-993d-e49adffb853d.jpg', 'https://youtu.be/FDgc8sZQYx0?si=ZnZJIcCytVPhYYvP', 'draft', 200),
-(1, 'Feria de Emprendedores', 'Exposición de startups y nuevos negocios', '2025-08-01 10:00:00', '2025-08-01 18:00:00', 'Plaza Mayor, Ciudad', 40.7128, -74.0060, 'private', 'Negocios', 'https://res.cloudinary.com/dlbvvaqsq/image/upload/v1747850920/79e2502c-7ede-415b-9a24-1311ff964260.jpg', 'https://youtu.be/a_gbE9A3Oz4?si=xO8FOR1k6hTC7Fac', 'published', 300),
-(1, 'Festival de Cine', 'Muestra de películas independientes', '2025-09-05 19:00:00', '2025-09-05 23:00:00', 'Calle de las Artes 789, Ciudad', 40.7128, -74.0060, 'invite-only', 'Cine', 'https://res.cloudinary.com/dlbvvaqsq/image/upload/v1747851335/e21b0b40-8aac-4984-9b66-e887b3cdb5ab.webp', 'https://youtu.be/pSy15uFkuME?si=Q9lLf0iWEX3-wfyb', 'published', 150),
+(2, 'Conferencia de Tecnología', 'Charlas sobre innovación tecnológica', '2025-07-15 09:00:00', '2025-07-15 17:00:00', 'Calle de la Innovación 456, Ciudad', 40.7128, -74.0060, 'public', 'Tecnología', 'https://res.cloudinary.com/dlbvvaqsq/image/upload/v1747850867/1e6236c8-d88d-43a6-993d-e49adffb853d.jpg', 'https://youtu.be/FDgc8sZQYx0?si=ZnZJIcCytVPhYYvP', 'published', 200),
+(1, 'Feria de Emprendedores', 'Exposición de startups y nuevos negocios', '2025-08-01 10:00:00', '2025-08-01 18:00:00', 'Plaza Mayor, Ciudad', 40.7128, -74.0060, 'public', 'Negocios', 'https://res.cloudinary.com/dlbvvaqsq/image/upload/v1747850920/79e2502c-7ede-415b-9a24-1311ff964260.jpg', 'https://youtu.be/a_gbE9A3Oz4?si=xO8FOR1k6hTC7Fac', 'published', 300),
+(1, 'Festival de Cine', 'Muestra de películas independientes', '2025-09-05 19:00:00', '2025-09-05 23:00:00', 'Calle de las Artes 789, Ciudad', 40.7128, -74.0060, 'public', 'Cine', 'https://res.cloudinary.com/dlbvvaqsq/image/upload/v1747851335/e21b0b40-8aac-4984-9b66-e887b3cdb5ab.webp', 'https://youtu.be/pSy15uFkuME?si=Q9lLf0iWEX3-wfyb', 'published', 150),
 (2, 'Exposición de Arte', 'Arte contemporáneo en la galería', '2025-05-25 17:00:00', '2025-05-25 21:00:00', 'Calle del Arte 321, Ciudad', 40.7128, -74.0060, 'public', 'Arte', 'https://res.cloudinary.com/dlbvvaqsq/image/upload/v1747851360/c0544c0d-5f09-41fe-9920-2a1bae1457a8.avif', 'https://youtu.be/P8v5-DCN-Q4?si=-xfva_Ixnf_vIbU7', 'published', 100);
 
 -- Insertar registros en la tabla de Tickets
